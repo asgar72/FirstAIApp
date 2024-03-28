11820d6ffae405ae02676279f27e147f12c05c8f
asgar72 <72asgar72@gmail.com>
2024-03-29 01:19:11 +0530

    final commit

--- app/src/main/java/com/asgar72/firstaiapp/MainActivity.kt
+++ app/src/main/java/com/asgar72/firstaiapp/MainActivity.kt
@@ -3,12 +3,34 @@ package com.asgar72.firstaiapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.asgar72.firstaiapp.databinding.ActivityMainBinding
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        supportActionBar?.hide()
 
+        binding.btnSubmit.setOnClickListener {
+            val prompt = binding.eTPrompt.text.toString()
+
+            val generativeModel = GenerativeModel(
+                // For text-only input, use the gemini-pro model
+                modelName = "gemini-pro",
+                // Access your API key as a Build Configuration variable (see "Set up your API key" above)
+                apiKey = "AIzaSyAnyR7hJ2K22FM4mJe9_VFo_m1x5m-zcbM"
+            )
+            runBlocking {
+                val response = generativeModel.generateContent(prompt)
+                binding.tVResult.text = response.text
+
+            }
+        }
     }
 }
